4cf43876bd210f2ad1f35eeebdf58ff448508617
Mysterken <mysterken.dev@gmail.com>
2025-06-21 19:20:52 +0200

    Add User entity with UUID and timestamps, update security configuration

---
+++ src/Entity/Traits/UuidTrait.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Validator\Constraints\Uuid;
+
+trait UuidTrait
+{
+    #[ORM\Id]
+    #[ORM\Column(type: UuidType::NAME, unique: true)]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\CustomIdGenerator(class: UuidGenerator::class)]
+    private ?Uuid $id = null;
+
+    public function getId(): ?Uuid
+    {
+        return $this->id;
+    }
+}
