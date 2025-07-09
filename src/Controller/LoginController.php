2175bc36bd7b950581b26463ac195f6398502a37
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2025-07-09 11:31:44 +0200

    Add rate limiting

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,18 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
 use App\Entity\User;
 use App\Repository\UserRepository;
 
 final class LoginController extends AbstractController
 {
     #[Route('/api/register', name: 'register', methods: ['POST'])]
-    public function register(EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): JsonResponse
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher,
+    RateLimiterFactoryInterface $anonymousApiLimiter): JsonResponse
     {
+        $limiter = $anonymousApiLimiter->create($request->getClientIp());
+
+        if (false === $limiter->consume(1)->isAccepted()) {
+            throw new TooManyRequestsHttpException();
+        }
+        
         $userName = $_POST['username'];
         $email = $_POST['email'];
         $password = $_POST['password'];
@@ -34,8 +43,6 @@ final class LoginController extends AbstractController
             $newUser->setEmail($email);
             $hashPassword = $hasher->hashPassword($newUser, $password);
             $newUser->setPassword($hashPassword);
-            $newUser->setCreatedAt(new \DateTimeImmutable());
-            $newUser->setUpdatedAt(new \DateTime());
             $entityManager->persist($newUser);
             $entityManager->flush();
         } catch (\Exception $e) {
@@ -46,8 +53,8 @@ final class LoginController extends AbstractController
     }
 
     #[Route('/api/login_check', name: 'login_check', methods: ['POST'])]
-    public function login_check(UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, 
-    JWTTokenManagerInterface $JWTManager): JsonResponse
+    public function login_check(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, 
+    JWTTokenManagerInterface $JWTManager, RateLimiterFactoryInterface $loginLimiter): JsonResponse
     {
         $email = $_POST['email'];
         $password = $_POST['password'];
@@ -64,6 +71,10 @@ final class LoginController extends AbstractController
         if (!$user || !$passwordHasher->isPasswordValid($user, $password)) {
             return $this->json(['message' => 'Identifiants invalides.'], 400);
         }
+
+        $apiKey = $request->headers->get('apikey');
+        $limiter = $loginLimiter->create($apiKey);
+        $limiter->reserve(1)->wait();
 
         $token = $JWTManager->create($user);
         return $this->json(['token' => $token], 200);
