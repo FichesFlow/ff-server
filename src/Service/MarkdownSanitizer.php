817448440c84b1bdca1c03d4bace6d4ae72fa05e
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2025-09-05 21:00:32 +0200

    install bundle and add markdown sanitizer service

---
+++ src/Service/MarkdownSanitizer.php
@@ -0,0 +1,32 @@
+<?php
+
+use League\CommonMark\CommonMarkConverter;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
+
+final class MarkdownSanitizer
+{
+    public function __construct(
+        private CommonMarkConverter $md,
+        private HtmlSanitizerInterface $san
+    ) {}
+
+    public function toSafeHtml(string $markdown): string
+    {
+        $html = $this->md->convert($markdown)->getContent();
+
+        return $this->san->sanitize($html, [
+            'allow_safe_elements' => true,
+            'allowed_tags' => [
+                'p','h1','h2','h3','h4','em','strong','u','del','code','pre',
+                'ul','ol','li','blockquote','a','img','span','div','br',
+                'table','thead','tbody','tr','th','td',
+                'math','mi','mn','mo','msup','msub','mrow'
+            ],
+            'allowed_attributes' => [
+                'a.href','a.title','a.target','a.rel',
+                'img.src','img.alt','img.title','img.width','img.height',
+                '*.class','*.style','math.xmlns'
+            ],
+        ]);
+    }
+}
