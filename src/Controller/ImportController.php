d08f596699f44c0a05657bf02303b0d616096b07
PatRider64 <83703900+PatRider64@users.noreply.github.com>
2025-09-13 23:10:24 +0200

    move part of the code

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -152,12 +152,12 @@ class ImportController extends AbstractController
                     $back = '';
                 }
             }
-        }
 
-        fclose($file);
+            fclose($file);
 
-        if (!empty($front) && empty($back)) { // Missing back side for last card
-            return $this->json(['message' => 'Incomplete card: missing back side or separator'], 422);
+            if (!empty($front) && empty($back)) { // Missing back side for last card
+                return $this->json(['message' => 'Incomplete card: missing back side or separator'], 422);
+            }
         }
 
         if ($cardCount == 0) {
